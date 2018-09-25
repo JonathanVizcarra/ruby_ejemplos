06bade7efeb35663c3e3694d2a9ec14b986e9fe6
JonathanVizcarra <josavizcarraca@ittepic.edu.mx>
2018-09-25 06:53:01 +0000

    feat: add tareas arreglo y hash

---
+++ mis_programas/tarea_arreglo.rb
@@ -0,0 +1,15 @@
+arreglo = Array.new
+arreglo = []
+
+arreglo[6] = 6
+arreglo.push(1)
+arreglo.push 1
+arreglo << 4
+
+arreglo.delete_at(2)
+arreglo.delete(4)
+arreglo.pop()
+arreglo.pop
+
+secuencia = (1..20)     #
+secuencia = (1...20)
---
+++ mis_programas/tarea_hash.rb
@@ -0,0 +1,7 @@
+hash = Hash.new
+
+hash[:valor1] = {subvalor: "valor del subvalor"}
+
+hash.delete(:valor1)
+
+hash[valor1].delete(subvalor) #
